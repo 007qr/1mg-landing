16c9869e020a269a585c2906c4e4dcd60b8b0a9f
Ayush Patil <Ayush Patil>
2025-03-31 23:12:39 +0530

    basic skeleton done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Wix_Madefor_Display } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const wix = Wix_Madefor_Display({
+  variable: "--font-wix",
   subsets: ["latin"],
 });
 
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${wix.className} antialiased`}
       >
         {children}
       </body>
